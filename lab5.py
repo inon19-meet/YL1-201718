e22608857a3a36413ffba8e9e02beca339107319
inon19-meet <inon19@meet.mit.edu>
2017-11-22 19:27:49 +0200

    COMMENT

---
+++ lab5.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+import random
+
+class Square(Turtle):
+    def __init__(self,color,size):
+        
+        Turtle.__init__(self)
+        
+        self.color(color)
+        self.shapesize(size)
+        self.shape("square")
+random.choice(['green'],['blue'],['red'],['white'],['black'],['yellow'])*256
+        
+        
+    
+
+
+    
