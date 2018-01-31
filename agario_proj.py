e22f47845c6acb4131e3e088105b3550af37f7ff
inon19-meet <inon19@meet.mit.edu>
2018-01-31 19:58:46 +0200

    project

--- agario_proj.py
+++ agario_proj.py
@@ -1,4 +1,5 @@
 import turtle
+import math
 import time
 import random
 from ball import Ball
@@ -28,23 +29,23 @@ for i in range(NUMBER_OF_BALLS):
     color =(random.random(),random.random(),random.random())
     d1 = Ball(x,y,dx,dy,redius,color)
     BALLS.append(d1)
-
+turtle.update()
 def move_all_balls():
     for ball in BALLS:
-        ball.move(SCREEN_WIDTH,SCREEN_HEIGHT)
+        ball.Move(SCREEN_WIDTH,SCREEN_HEIGHT)
 
 def collide(ball1,ball2):
     if ball1==ball2:
         return False
     D = math.sqrt(math.pow(ball2.xcor()-ball1.xcor(),2)+math.pow(ball2.ycor()-ball1.ycor(),2))
-    summ = ball1.radius+ball2.radius
-    if(Dr>summ):
+    summ = ball1.r+ball2.r
+    if(D>summ):
         return False
-    if(Dr<summ):
+    if(D<summ):
         return True
         
     
-def check_colloision():
+def check_collision():
     for ball1 in BALLS:
         for ball2 in BALLS:
             if collide(ball1,ball2):
@@ -65,11 +66,11 @@ def check_colloision():
                     ball1.color = color
                 if br1>br2:
                      ball1.r=br1+1
-def check_myball_colloision():
+def check_myball_collision():
     for MY_Ball in BALLS:
         for ball in BALLS:
             if collide(MY_BALL,ball):
-                br11 = ball1.r
+                br11 = ball.r
                 brMB =  MY_BALL.r
                 x_cor = random.randint(int(-SCREEN_WIDTH) + MAXIMUM_BALL_RADIUS,int(SCREEN_WIDTH) - MAXIMUM_BALL_RADIUS)
                 y_cor = random.randint(int(-SCREEN_HEIGHT) + MAXIMUM_BALL_RADIUS,int(SCREEN_HEIGHT) - MAXIMUM_BALL_RADIUS)
@@ -78,22 +79,38 @@ def check_myball_colloision():
                 redius = random.randint(MINIMUM_BALL_RADIUS,MAXIMUM_BALL_RADIUS)
                 color =(random.random(),random.random(),random.random())
                 if brMB<br11:
-                    ball1.x = x_cor
-                    ball1.y = y_cor
-                    ball1.dx = dx_speed
-                    ball1.dy = dy_speed
-                    ball1.r = redius
-                    ball1.color = color
+                    ball.x = x_cor
+                    ball.y = y_cor
+                    ball.dx = dx_speed
+                    ball.dy = dy_speed
+                    ball.r = redius
+                    ball.color = color
                 if brMB>br11:
                      brMB=brMB+1
-                if(MY_BALL<ball1):
+                if(MY_BALL<ball):
                     return False
-return True
+
+    return True
+
+def movearound(event):
+    MY_BALL.goto(event.x-SCREEN_WIDTH,event.y-SCREEN_HEIGHT)
 
 
+turtle.getcanvas().bind("<Motion>", movearound)
+turtle.listen()
 
-
-
+while RUNNING == True:
+    if SCREEN_WIDTH!=turtle.getcanvas().winfo_width()/2 or SCREEN_HEIGHT != turtle.getcanvas().winfo_height()/2:
+        SCREEN_WIDTH=turtle.getcanvas().winfo_width()/2
+        SCREEN_HEIGHT = turtle.getcanvas().winfo_height()/2
+    move_all_balls()
+    check_collision()
+    MY_BALL.Move(SCREEN_WIDTH, SCREEN_HEIGHT)
+    #check_myball_collision()
+    
+    turtle.update()
+    time.sleep(SLEEP)
+    
 
                      
 
--- ball.py
+++ ball.py
@@ -7,29 +7,34 @@ class Ball(Turtle):
         self.dx = dx
         self.dy = dy
         self.r = r
+        self.pu()
+        self.goto(x,y)
         self.color(color)
         self.shapesize(r/10)
         self.shape("circle")
+        
 
     def Move(self,screen_width,screen_height):
-        current_x = self.x
-        current_y = self.y
+        current_x = self.xcor()
+        current_y = self.ycor()
         current_dx = self.dx
         current_dy = self.dy
+        new_r = self.r
         new_x = current_x+current_dx
         new_y = current_y+current_dy
 
-    def ball_right_side(self,right_side):
-        self.right_side = (x+r,y)
-    def ball_left_side(self,right_side):
-        self.left_side = (x-r,y)
-    def ball_up_side(self,up_side):
-        self.up_side = (x,y+r)
-    def ball_down_side(self,down_side):
-        self.down_side = (x,y-r)
+        self.goto(new_x, new_y)
         
+        ball_right_side = new_x+new_r
+        ball_left_side = new_x-new_r
+        ball_up_side = new_y+new_r
+        ball_down_side =new_y-new_r
+        
+        if ball_right_side>screen_width/2 or ball_left_side<-screen_width/2:
+            self.dx = (-1)*self.dx
 
-
+        if ball_up_side>screen_height/2 or ball_down_side<-screen_height/2:
+            self.dy = (-1)*self.dy
 
 
 
